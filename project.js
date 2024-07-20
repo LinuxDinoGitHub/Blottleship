14f6f988c2012550c24b4ce5344621f771e9ae6a
LinuxDinoGitHub <95269147+LinuxDinoGitHub@users.noreply.github.com>
2024-07-20 14:01:58 +0800

    debugged finally but position is funny

--- project.js
+++ project.js
@@ -14,34 +14,7 @@ setDocDimensions(width, height);
 
 //Title code - BATTLESHIP
 
-const letters = { //tysm Scott C who made the following letter commands for his crossword game this saved me so much time, huge credits to them
-  "a": `sa$90,f$2,r$90,f$2,r$90,f$2,u,sa$90,f$2,d,l$30,f$2,l$120,f$2`,
-  "b": `sa$90,f$4,r$90,f$1,arc$180:1,f$1,r$180,f$1,arc$180:1,f$1`,
-  "c": `sa$90,u,r$90,f$2,r$180,d,arc$180:2`,
-  "d": `sa$90,f$4,r$90,arc$180:2`,
-  "e": `sa$90,f$4,r$90,f$2,u,f$-2,r$90,f$2,l$90,d,f$2,u,f$-2,r$90,f$2,l$90,d,f$2`,
-  "f": `sa$90,f$4,r$90,f$2,u,f$-2,r$90,f$2,l$90,d,f$2`,
-  "g": `u,f$1,sa$90,f$2,r$90,d,arc$270:1,f$2,arc$90:1`,
-  "h": `sa$90,f$4,u,f$-2,r$90,d,f$2,u,l$90,f$-2,d,f$4`,
-  "i": `f$2,u,f$-1,l$90,d,f$4,r$90,u,f$-1,d,f$2`,
-  "j": `sa$90,u,f$4,r$90,d,f$2,u,f$-1,r$90,d,f$3,arc$90:1`,
-  "k": `sa$90,f$4,u,f$-2,r$45,d,f$2.75,u,f$-2.75,r$90,d,f$2.75`,
-  "l": `sa$90,u,f$4,r$180,d,f$4,l$90,f$2`,
-  "m": `sa$90,f$4,sa$0,r$71.57,f$3.162,sa$0,l$71.57,f$3.162,sa$0,r$90,f$4`,
-  "n": `sa$90,f$4,r$153.43,f$4.47,l$153.43,f$4`,
-  "o": `sa$90,u,f$1,d,f$2,arc$180:1,f$2,arc$180:1`,
-  "p": `sa$90,f$4,r$90,f$1,arc$180:1,f$1`,
-  "q": `sa$90,u,f$1,d,f$2,arc$180:1,f$2,arc$180:1,u,r$90,f$1,r$45,d,f$1.414`,
-  "r": `sa$90,f$4,r$90,f$1,arc$180:1,f$1,sa$-45,f$2.8284`,
-  "s": `f$1,arc$-180:1,arc$180:1,f$1`,
-  "t": `u,f$1,sa$90,d,f$4,u,r$90,f$-1,d,f$2`,
-  "u": `sa$90,u,f$4,sa$-90,d,f$3,arc$-180:1,f$3`,
-  "v": `sa$90,u,f$4,r$165.96,d,f$4.12,l$151.93,f$4.12`,
-  "w": `sa$90,u,f$4,sa$0,r$82.87,d,f$4.03,sa$0,l$63.43,f$1.12,sa$0,r$63.43,f$1.12,sa$0,l$82.87,f$4.03`,
-  "x": `sa$90,u,f$4,r$153.44,d,f$4.47,u,l$153.44,f$4,l$153.44,d,f$4.47`,
-  "y": `u,f$1,sa$90,d,f$2.5,r$33.69,f$1.8,u,f$-1.8,l$67.38,d,f$1.8`,
-  "z": `u,f$2,d,f$-2,l$63.44,f$4.47,r$63.44,f$-2`,
-}
+
 
 function drawLetter(letter, posx, posy){ //Text engine essentially
   const turtle = new bt.Turtle();
@@ -74,15 +47,49 @@ function drawLetter(letter, posx, posy){ //Text engine essentially
     }
   }
   drawLines(turtle.lines());
+  console.log(turtle.position);
   return turtle.position;
+}
+
+const letters = { //tysm Scott C who made the following letter commands for his crossword game this saved me so much time, huge credits to them
+  "a": `sa$90,f$2,r$90,f$2,r$90,f$2,u,sa$90,f$2,d,l$30,f$2,l$120,f$2`,
+  "b": `sa$90,f$4,r$90,f$1,arc$180:1,f$1,r$180,f$1,arc$180:1,f$1`,
+  "c": `sa$90,u,r$90,f$2,r$180,d,arc$180:2`,
+  "d": `sa$90,f$4,r$90,arc$180:2`,
+  "e": `sa$90,f$4,r$90,f$2,u,f$-2,r$90,f$2,l$90,d,f$2,u,f$-2,r$90,f$2,l$90,d,f$2`,
+  "f": `sa$90,f$4,r$90,f$2,u,f$-2,r$90,f$2,l$90,d,f$2`,
+  "g": `u,f$1,sa$90,f$2,r$90,d,arc$270:1,f$2,arc$90:1`,
+  "h": `sa$90,f$4,u,f$-2,r$90,d,f$2,u,l$90,f$-2,d,f$4`,
+  "i": `f$2,u,f$-1,l$90,d,f$4,r$90,u,f$-1,d,f$2`,
+  "j": `sa$90,u,f$4,r$90,d,f$2,u,f$-1,r$90,d,f$3,arc$90:1`,
+  "k": `sa$90,f$4,u,f$-2,r$45,d,f$2.75,u,f$-2.75,r$90,d,f$2.75`,
+  "l": `sa$90,u,f$4,r$180,d,f$4,l$90,f$2`,
+  "m": `sa$90,f$4,sa$0,r$71.57,f$3.162,sa$0,l$71.57,f$3.162,sa$0,r$90,f$4`,
+  "n": `sa$90,f$4,r$153.43,f$4.47,l$153.43,f$4`,
+  "o": `sa$90,u,f$1,d,f$2,arc$180:1,f$2,arc$180:1`,
+  "p": `sa$90,f$4,r$90,f$1,arc$180:1,f$1`,
+  "q": `sa$90,u,f$1,d,f$2,arc$180:1,f$2,arc$180:1,u,r$90,f$1,r$45,d,f$1.414`,
+  "r": `sa$90,f$4,r$90,f$1,arc$180:1,f$1,sa$-45,f$2.8284`,
+  "s": `f$1,arc$-180:1,arc$180:1,f$1`,
+  "t": `u,f$1,sa$90,d,f$4,u,r$90,f$-1,d,f$2`,
+  "u": `sa$90,u,f$4,sa$-90,d,f$3,arc$-180:1,f$3`,
+  "v": `sa$90,u,f$4,r$165.96,d,f$4.12,l$151.93,f$4.12`,
+  "w": `sa$90,u,f$4,sa$0,r$82.87,d,f$4.03,sa$0,l$63.43,f$1.12,sa$0,r$63.43,f$1.12,sa$0,l$82.87,f$4.03`,
+  "x": `sa$90,u,f$4,r$153.44,d,f$4.47,u,l$153.44,f$4,l$153.44,d,f$4.47`,
+  "y": `u,f$1,sa$90,d,f$2.5,r$33.69,f$1.8,u,f$-1.8,l$67.38,d,f$1.8`,
+  "z": `u,f$2,d,f$-2,l$63.44,f$4.47,r$63.44,f$-2`,
 }
 
 let text = "battleship"
 text = text.split('')
 let lastCordX = 20
 let lastCordY = 100
+let currCordX = 20
+let currCordY = 100
 for(const letter of text){
-  [lastCordX, lastCordY] = [drawLetter(letter, lastCordX + 28, lastCordY)];
+  [lastCordX, lastCordY] = [currCordX, currCordY];
+  console.log("COords",lastCordX, lastCordY);
+  [currCordX, currCordY] = drawLetter(letter, lastCordX, lastCordY);
 }
 const frame = [];
 
